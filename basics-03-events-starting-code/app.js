ca4f929034fdf41cf6be1fad0f94b01fd48478fa
niteshchandra7 <nitesh240295@gmail.com>
2025-05-01 15:18:26 +0530

    basics-events

---
+++ basics-03-events-starting-code/app.js
@@ -0,0 +1,28 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: "",
+      confirmedName: "",
+    };
+  },
+  methods: {
+    add(num) {
+      return (this.counter += num);
+    },
+    reduce(num) {
+      this.counter -= num;
+    },
+    setName(event) {
+      this.name = event.target.value;
+    },
+    submitForm() {
+      alert("Submitted!");
+    },
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
+  },
+});
+
+app.mount("#events");
