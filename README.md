32037b99fdf4aab7cc41d6961b778eaff1ada9e0
niteshchandra7 <nitesh240295@gmail.com>
2025-05-02 20:36:31 +0530

    computed,watchers & methods

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # VUE
+
+# methods
+## Use with event binding or data binding
+## Data Binding : Method is executed for every re-render cycle of the component
+## Use for events or data that really needs to be re-evaluated all the time
+
+# computed
+## Use with data binding
+## Computed properties are only re-evaluated if one of their "used value" changes
+## Use for data that depends on other data
+
+# watch
+## Not used directly in template
+## Allows you to run any code in reaction to some changed data 
+## Ex. send http request etc..
+## Use for any non-data update you want to make
+
+
+# computed vs methods
+## We should prefer computed vs method since it only runs when there is a change in dependency
+## while method runs everytime an update is made. But we can't avoid method when there is event as an argument or 
+## when we want the function to run eacah time when there is an update
+## No parenthesis when calling computed functions
+## Note: you still binds your events to methods not compued properties.
+## Use computed properties for outputing something.
+
+# watchers
+## it is good for watching a single attribute if multiple attributes needs to be handled compute is preferred.
+
+
+
--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -3,10 +3,44 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: "",
-      confirmedName: "",
+      //fullName: "",
+      lastName: "",
     };
   },
+  watch: {
+    counter(value) {
+      if (value > 50) {
+        const that = this;
+        setTimeout(function () {
+          that.counter = 0;
+        }, 2000);
+      }
+    },
+    //name(newvalue, oldvalue) {
+    // name(value) {
+    //   // logic that shouold run when name attribute changes
+    //   // this.fullname = this.name + " " + "Chandra";
+    //   if (value === "") this.fullName = value; // better way
+    //   else this.fullName = value + " " + this.lastName;
+    // },
+    // lastName(value) {
+    //   if (value === "") this.fullName = ""; // better way
+    //   else this.fullName = this.name + " " + value;
+    // },
+  },
+  computed: {
+    fullName() {
+      console.log("outputFullName called");
+      if (this.name === "" || this.lastName === "") return "";
+      return this.name + " " + this.lastName;
+    },
+  },
   methods: {
+    // outputFullName() {
+    //   console.log("outputFullName called");
+    //   if (this.name === "") return "";
+    //   return this.name + " " + "Chandra";
+    // },
     add(num) {
       return (this.counter += num);
     },
@@ -20,7 +54,11 @@ const app = Vue.createApp({
       alert("Submitted!");
     },
     confirmInput() {
-      this.confirmedName = this.name;
+      this.fullname = this.name;
+    },
+    resetInput() {
+      console.log("hello world");
+      this.name = "";
     },
   },
 });
